9e989ab216e3bfd4f1fb24ae4fcfbf2529d4d4b9
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-06-12 17:53:48 +0300

    first commit

---
+++ JS-Syntax-Fundamentals/Lab/02.StudentInformation.js
@@ -0,0 +1,7 @@
+function printInfo(name, age, avgGrade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${avgGrade.toFixed(2)}`);
+}
+
+printInfo('John', 15, 5.54678);
+printInfo('Steve', 16, 2.1426);
+printInfo('Marry', 12, 6.00);
