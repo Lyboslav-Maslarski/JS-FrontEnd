575a7594524b20bfdc0c5468201e1e5467d1597f
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-06-12 19:18:42 +0300

    add

--- JS-Syntax-Fundamentals/Lab/15. Censored Words.js
+++ JS-Syntax-Fundamentals/Lab/15. Censored Words.js
@@ -2,8 +2,5 @@ function solve(text, word) {
     while (text.includes(word)) {
         text = text.replace(word, '*'.repeat(word.length));
     }
-
     console.log(text);
 }
-
-solve('A small sentence with some words', 'small');
