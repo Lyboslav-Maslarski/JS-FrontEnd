ef72a18c15333c998b1d87491318e3e2810c954e
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-06-16 22:16:42 +0300

    add

--- Functions-and-Statements/More Exercise/05. Print DNA.js
+++ Functions-and-Statements/More Exercise/05. Print DNA.js
@@ -1,3 +1,20 @@
-function pointsValidation(x1, y1, x2, y2) {
-
+function printDNA(length) {
+    let index = 0;
+    let chars = ['AT', 'CG', 'TT', 'AG', 'GG'];
+    for (let i = 0; i < length; i++) {
+        if (index === 0) {
+            index++;
+            console.log(`**${chars[i % chars.length]}**`);
+        } else if (index === 1) {
+            index++;
+            console.log(`*${chars[i % chars.length].charAt(0)}--${chars[i % chars.length].charAt(1)}*`);
+        } else if (index === 2) {
+            index++;
+            console.log(`${chars[i % chars.length].charAt(0)}----${chars[i % chars.length].charAt(1)}`);
+        } else {
+            index++;
+            console.log(`*${chars[i % chars.length].charAt(0)}--${chars[i % chars.length].charAt(1)}*`);
+            index = 0;
+        }
+    }
 }
