37dba699c90ff517c3b6d315c64f7dbf1dc8efb9
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-06-18 21:55:53 +0300

    add

---
+++ Objects and Classes/Exercise/05. Inventory.js
@@ -0,0 +1,21 @@
+function registerHeroes(input) {
+    let heroes = [];
+    for (const line of input) {
+        let [heroName, level, items] = line.split(' / ');
+        let hero = { heroName, level: Number(level), items };
+        heroes.push(hero);
+    }
+    heroes.sort((f, s) => f.level - s.level)
+        .forEach(h => {
+            console.log(`Hero: ${h.heroName}`);
+            console.log(`level => ${h.level}`);
+            console.log(`items => ${h.items}`);
+        })
+}
+
+registerHeroes([
+    'Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara'
+]
+)
