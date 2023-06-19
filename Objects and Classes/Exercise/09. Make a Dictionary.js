173854ece3a71dc9f2104f61f66a9bab0dfbb7cf
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-06-19 19:11:02 +0300

    add

--- Objects and Classes/Exercise/09. Make a Dictionary.js
+++ Objects and Classes/Exercise/09. Make a Dictionary.js
@@ -1,3 +1,12 @@
-function name(params) {
-    
+function makeDictionary(input) {
+    let dictionary = {};
+    for (const line of input) {
+        let obj = JSON.parse(line);
+        let term = Object.keys(obj)[0];
+        let definition = Object.values(obj)[0];
+        dictionary[term] = definition;
+    }
+    Object.entries(dictionary)
+        .sort(([fKey, fValue], [sKey, sValue]) => fKey.localeCompare(sKey))
+        .forEach(([key, value]) => console.log(`Term: ${key} => Definition: ${value}`));
 }
