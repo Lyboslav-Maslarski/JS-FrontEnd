43bea5559aa8de9940259c2e3fbe8063528dc2b2
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-06-21 22:16:21 +0300

    add

--- Objects and Classes/More Exercise/09. Armies.js
+++ Objects and Classes/More Exercise/09. Armies.js
@@ -3,34 +3,47 @@ function armiesParse(input) {
     for (const line of input) {
         if (line.includes('arrives')) {
             let leaderName = line.substring(0, line.indexOf('arrives') - 1);
-            let leader = { name: leaderName, armyCount: 0, armies: [] };
+            let leader = { name: leaderName, totalArmyCount: 0, armies: [] };
             leaders.push(leader);
         } else if (line.includes(':')) {
             let [leaderName, armyInfo] = line.split(': ');
             let [armyName, armyCount] = armyInfo.split(', ');
             let currentLeader = leaders.find(l => l.name === leaderName);
             if (currentLeader) {
-                currentLeader.armyCount += Number(armyCount);
+                armyCount = Number(armyCount);
+                currentLeader.totalArmyCount += armyCount;
                 let army = { armyName, armyCount };
                 currentLeader.armies.push(army);
             }
+        } else if (line.includes('+')) {
+            let [searchedArmyName, addedCount] = line.split(' + ');
+            for (const leader of leaders) {
+                for (const army of leader.armies) {
+                    if (army.armyName === searchedArmyName) {
+                        army.armyCount += Number(addedCount);
+                        leader.totalArmyCount += Number(addedCount);
+                    }
+                }
+            }
         } else {
-            let [armyName, addedCount] = line.split(' + ');
-            
+            let leaderName = line.substring(0, line.indexOf('defeated') - 1);
+            let leaderIndex = -1;
+            for (let i = 0; i < leaders.length; i++) {
+                if (leaders[i].name === leaderName) {
+                    leaderIndex = i;
+                }
+            }
+            if (leaderIndex >= 0) {
+                leaders.splice(leaderIndex, 1);
+            }
         }
     }
-}
 
-armiesParse(['Rick Burr arrives',
-    'Fergus: Wexamp, 30245',
-    'Rick Burr: Juard, 50000',
-    'Findlay arrives',
-    'Findlay: Britox, 34540',
-    'Wexamp + 6000',
-    'Juard + 1350',
-    'Britox + 4500',
-    'Porter arrives',
-    'Porter: Legion, 55000',
-    'Legion + 302',
-    'Rick Burr defeated',
-    'Porter: Retix, 3205'])
+    leaders = leaders.sort((f, s) => s.totalArmyCount - f.totalArmyCount);
+
+    for (const leader of leaders) {
+        console.log(`${leader.name}: ${leader.totalArmyCount}`);
+        leader.armies.sort((f, s) => s.armyCount - f.armyCount)
+            .forEach(army => console.log(`>>> ${army.armyName} - ${army.armyCount}`));
+    }
+}
