f7f78680e49c70fcfef4e625b35ba9713a98da88
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-06-22 18:08:00 +0300

    add

--- Objects and Classes/More Exercise/02. Catalogue.js
+++ Objects and Classes/More Exercise/02. Catalogue.js
@@ -20,15 +20,3 @@ function createCatalog(input) {
             .join('\n'));
     }
 }
-
-createCatalog([
-    'Appricot : 20.4',
-    'Fridge : 1500',
-    'TV : 1499',
-    'Deodorant : 10',
-    'Boiler : 300',
-    'Apple : 1.25',
-    'Anti-Bug Spray : 15',
-    'T-Shirt : 10'
-]
-)
--- Objects and Classes/More Exercise/04. Flight Schedule.js
+++ Objects and Classes/More Exercise/04. Flight Schedule.js
@@ -25,21 +25,3 @@ function flightScheduler(input) {
         .map(flight => ({ Destination: flight.destination, Status: flight.status }))
         .forEach(flight => console.log(flight));
 }
-
-flightScheduler([['WN269 Delaware',
-    'FL2269 Oregon',
-    'WN498 Las Vegas',
-    'WN3145 Ohio',
-    'WN612 Alabama',
-    'WN4010 New York',
-    'WN1173 California',
-    'DL2120 Texas',
-    'KL5744 Illinois',
-    'WN678 Pennsylvania'],
-['DL2120 Cancelled',
-    'WN612 Cancelled',
-    'WN1173 Cancelled',
-    'SK430 Cancelled'],
-['Ready to fly']
-]
-)
