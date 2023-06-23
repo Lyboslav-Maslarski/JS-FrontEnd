f83e0556cdb73c16381d6a6e2ecc13ed4923dbc1
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-06-23 21:37:58 +0300

    add

--- DOM and Events/Lab/09. Highlight-Active/app.js
+++ DOM and Events/Lab/09. Highlight-Active/app.js
@@ -1,3 +1,15 @@
 function focused() {
-    console.log('TODO:...');
+    let inputs = document.querySelectorAll('input[type="text"]');
+    for (const i of inputs) {
+        i.addEventListener('focus', addClass);
+        i.addEventListener('blur', removeClass);
+    }
+
+    function addClass(e) {
+        e.target.parentNode.classList.add('focused');
+    }
+
+    function removeClass(e) {
+        e.target.parentNode.classList.remove('focused');
+    }
 }
