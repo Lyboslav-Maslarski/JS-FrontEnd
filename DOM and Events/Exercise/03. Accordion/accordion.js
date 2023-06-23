54faee6640f032e6ecf52c4d3f5d704e7ff29f8e
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-06-24 00:08:25 +0300

    add

--- DOM and Events/Exercise/03. Accordion/accordion.js
+++ DOM and Events/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let btn = document.getElementsByClassName('button')[0];
+    let content = document.getElementById('extra');
+
+    if (content.style.display === "none") {
+        content.style.display = "block"
+        btn.textContent = "Less"
+    } else {
+        content.style.display = "none"
+        btn.textContent = "More"
+    }
 }
--- DOM and Events/Exercise/04. Locked-Profile/app.js
+++ DOM and Events/Exercise/04. Locked-Profile/app.js
@@ -1,3 +1,22 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const btns = document.getElementsByTagName('button');
+    [...btns].forEach(btn => btn.addEventListener('click', showOrHideContent));
+
+
+    function showOrHideContent(event) {
+        const btn = event.target;
+        const profile = btn.parentNode;
+        const unlocked = profile.querySelector('input[value="unlock"]');
+        const hiddenContent = profile.getElementsByTagName('div')[0];
+
+        if (unlocked.checked) {
+            if (btn.textContent === 'Show more') {
+                btn.textContent = 'Hide it'
+                hiddenContent.style.display = 'inline-block';
+            } else {
+                btn.textContent = 'Show more'
+                hiddenContent.style.display = 'none';
+            }
+        }
+    }
 }
