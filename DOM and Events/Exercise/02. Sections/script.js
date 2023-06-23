25ccb85d969e30a6ab505b5aaa7448d95bd414e1
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-06-23 22:29:01 +0300

    add

--- DOM and Events/Exercise/02. Sections/script.js
+++ DOM and Events/Exercise/02. Sections/script.js
@@ -1,3 +1,15 @@
 function create(words) {
-   console.log('TODO:...');
+   const mainContainer = document.getElementById('content');
+
+   for (const word of words) {
+      let container = document.createElement('div');
+      let paragraph = document.createElement('p');
+      paragraph.textContent = word;
+      paragraph.style.display = 'none';
+
+      container.appendChild(paragraph);
+      container.addEventListener('click', () => paragraph.style.display = 'block');
+
+      mainContainer.appendChild(container);
+   }
 }
