aab70f1a4c95ed977a018fd2bbe14aed6df0bd83
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-06-23 20:26:15 +0300

    add

--- DOM and Events/Lab/05. Delete-From-Table/app.js
+++ DOM and Events/Lab/05. Delete-From-Table/app.js
@@ -1,3 +1,13 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let result = document.getElementById('result');
+    let input = document.querySelector('input[type="text"]');
+    let tableRow = [...document.querySelectorAll('td:nth-child(even)')]
+        .find(td => td.textContent === input.value);
+
+    if (tableRow) {
+        tableRow.parentElement.remove();
+        result.textContent = 'Deleted.';
+    } else {
+        result.textContent = 'Not found.';
+    }
 }
--- DOM and Events/Lab/06. Add-Delete/app.js
+++ DOM and Events/Lab/06. Add-Delete/app.js
@@ -1,3 +1,20 @@
 function addItem() {
-    //TODO...
+    const ulContainer = document.getElementById('items');
+    const input = document.getElementById('newItemText');
+
+    const newLi = document.createElement('li');
+    const anchor = document.createElement('a');
+    anchor.addEventListener('click', deleteElement);
+    anchor.textContent = '[Delete]';
+    anchor.href = '#';
+    newLi.textContent = input.value;
+
+    newLi.appendChild(anchor);
+    ulContainer.appendChild(newLi);
+
+    input.value = '';
+
+    function deleteElement(e) {
+        e.currentTarget.parentElement.remove();
+    }
 }
