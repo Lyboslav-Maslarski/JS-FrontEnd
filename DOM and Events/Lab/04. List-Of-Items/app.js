b87fd1d9871555ac27c533581d14061966e95cae
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-06-23 18:50:49 +0300

    add

--- DOM and Events/Lab/04. List-Of-Items/app.js
+++ DOM and Events/Lab/04. List-Of-Items/app.js
@@ -1,3 +1,10 @@
 function addItem() {
-    console.log('TODO:...');
+    const ulContainer = document.getElementById('items');
+    const input = document.getElementById('newItemText');
+    
+    let newLi = document.createElement('li');
+    newLi.textContent = input.value;
+    ulContainer.appendChild(newLi);
+
+    input.value = '';
 }
