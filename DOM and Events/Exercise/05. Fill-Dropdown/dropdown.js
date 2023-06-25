eff8fc532a009a33dcbb05c97189e915d27309eb
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-06-25 18:17:18 +0300

    add

--- DOM and Events/Exercise/05. Fill-Dropdown/dropdown.js
+++ DOM and Events/Exercise/05. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    const menu = document.getElementById('menu');
+    const newItemText = document.getElementById('newItemText');
+    const newItemValue = document.getElementById('newItemValue');
+
+    let newOption = document.createElement('option');
+    newOption.textContent = newItemText.value;
+    newOption.value = newItemValue.value;
+
+    menu.appendChild(newOption);
+    newItemText.value = '';
+    newItemValue.value = '';
 }
--- DOM and Events/Exercise/06. Table - Search Engine/solution.js
+++ DOM and Events/Exercise/06. Table - Search Engine/solution.js
@@ -2,7 +2,17 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const searchField = document.getElementById('searchField');
+      const tableRows = Array.from(document.querySelectorAll('tbody tr'));
+      
+      for (const row of tableRows) {
+         if (row.textContent.includes(searchField.value)) {
+            row.classList.add('select');
+         } else if (row.classList.contains('select')) {
+            row.classList.remove('select');
+         }
+      }
 
+      searchField.value = '';
    }
 }
