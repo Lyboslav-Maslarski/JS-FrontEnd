0b9fd5446062312f2522e14d0a794325aefd80a7
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-06-25 18:46:41 +0300

    add

--- DOM and Events/Exercise/07. Format the Text/solution.js
+++ DOM and Events/Exercise/07. Format the Text/solution.js
@@ -1,3 +1,14 @@
 function solve() {
-  //TODO
+  const input = document.getElementById('input');
+  const output = document.getElementById('output');
+  let sentences = input.value.split('.').filter(s => s !== '');
+
+  while (sentences.length > 0) {
+    let currentSentences = sentences.splice(0, 3);
+    let text = currentSentences.map(s => s = s.trim()).join('. ') + '.';
+
+    let newP = document.createElement('p');
+    newP.textContent = text;
+    output.appendChild(newP);
+  }
 }
