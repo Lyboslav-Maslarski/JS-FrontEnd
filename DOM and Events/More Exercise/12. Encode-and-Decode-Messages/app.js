bad199bc8328fea556c8514032dc9ffc2e1c0e14
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-06-26 21:55:53 +0300

    add

--- DOM and Events/More Exercise/12. Encode-and-Decode-Messages/app.js
+++ DOM and Events/More Exercise/12. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,25 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let [sendTextArea, receiveTextArea] = Array.from(document.getElementsByTagName('textarea'));
+    let [encodeBtn, decodeBtn] = Array.from(document.getElementsByTagName('button'));
+
+    encodeBtn.addEventListener('click', encode);
+    decodeBtn.addEventListener('click', decode);
+
+
+    function encode() {
+        let result = '';
+        for (const letter of sendTextArea.value) {
+            result += String.fromCharCode(letter.charCodeAt(0) + 1);
+        }
+        sendTextArea.value = '';
+        receiveTextArea.value = result;
+    }
+
+    function decode() {
+        let result = '';
+        for (const letter of receiveTextArea.value) {
+            result += String.fromCharCode(letter.charCodeAt(0) - 1);
+        }
+        receiveTextArea.value = result;
+    }
 }
--- DOM and Events/More Exercise/13. Distance-Converter/distanceConverter.js
+++ DOM and Events/More Exercise/13. Distance-Converter/distanceConverter.js
@@ -1,3 +1,26 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    const input = document.getElementById('inputDistance');
+    const inputSelect = document.getElementById('inputUnits');
+
+    const output = document.getElementById('outputDistance');
+    const outputSelect = document.getElementById('outputUnits');
+
+    const btn = document.getElementById('convert');
+
+    btn.addEventListener('click', convert);
+
+    function convert() {
+        let rates = {
+            km: 1000,
+            m: 1,
+            cm: 0.01,
+            mm: 0.001,
+            mi: 1609.34,
+            yrd: 0.9144,
+            ft: 0.3048,
+            in: 0.0254
+        }
+        let number = Number(input.value) * rates[inputSelect.value];
+        output.value = number / rates[outputSelect.value]
+    }
 }
