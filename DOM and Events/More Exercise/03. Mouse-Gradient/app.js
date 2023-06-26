c4009e959acb4f781fed0dd826b5f50257fa2d61
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-06-26 07:52:41 +0300

    add

--- DOM and Events/More Exercise/03. Mouse-Gradient/app.js
+++ DOM and Events/More Exercise/03. Mouse-Gradient/app.js
@@ -1,3 +1,16 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const container = document.getElementById('gradient');
+    const result = document.getElementById('result');
+
+    container.addEventListener('mousemove', showGradient)
+    container.addEventListener('mouseleave', hideResult);
+
+    function showGradient(e) {
+        let offset = e.offsetX;
+        let containerWidth = container.clientWidth;
+        result.textContent = Math.floor((offset / containerWidth) * 100) + '%';
+    }
+    function hideResult() {
+        result.textContent = '';
+    }
 }
--- DOM and Events/More Exercise/04. Dynamic-Validation/app.js
+++ DOM and Events/More Exercise/04. Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    console.log('TODO:...');
+    let input = document.getElementById('email');
+    input.addEventListener('change', checkForValidity);
+
+    function checkForValidity() {
+        if (input.value.match(/[a-z]@[a-z].[a-z]/)) {
+            input.classList.remove('error');
+        } else {
+            input.classList.add('error');
+        }
+    }
 }
