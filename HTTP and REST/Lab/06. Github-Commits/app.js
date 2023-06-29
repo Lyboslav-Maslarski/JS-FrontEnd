554e875f5ac503931df80e860f9573c7ed818a86
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-06-29 21:48:01 +0300

    add

--- HTTP and REST/Lab/06. Github-Commits/app.js
+++ HTTP and REST/Lab/06. Github-Commits/app.js
@@ -1,4 +1,21 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+    const username = document.getElementById('username').value;
+    const repo = document.getElementById('repo').value;
+    const list = document.getElementById('commits');
+    const URL = `https://api.github.com/repos/${username}/${repo}/commits`;
+
+    try {
+        const allCommitsRes = await fetch(URL);
+        const data = await allCommitsRes.json();
+        data.forEach(c => {
+            let newLi = document.createElement('li');
+            newLi.textContent = `${c.commit.author.name}: ${c.commit.message}`;
+            list.appendChild(newLi);
+        })
+    } catch (err) {
+        let newLi = document.createElement('li');
+        console.log(err);
+        newLi.textContent = `Error: ${err.status} (Not Found)`;
+        list.appendChild(newLi);
+    }
 }
