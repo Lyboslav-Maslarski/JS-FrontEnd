76c31ee5abfc7a237df9304bbad7bbc5791e841d
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-06-29 20:49:16 +0300

    add

--- HTTP and REST/Lab/04. GitHub-Repos/scripts/app.js
+++ HTTP and REST/Lab/04. GitHub-Repos/scripts/app.js
@@ -1,3 +1,8 @@
 function loadRepos() {
-   console.log("TODO...");
+   const URL = 'https://api.github.com/users/testnakov/reposs';
+   const result = document.getElementById('res');
+   fetch(URL, { method: 'GET' })
+      .then(res => res.text())
+      .then(data => result.textContent = data)
+      .catch(err => console.error(err));
 }
