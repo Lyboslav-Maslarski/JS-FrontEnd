34aa40e6a9310ea87a5b295e6f4090d42786681e
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-06-30 17:20:25 +0300

    add

--- HTTP and REST/Exercise/01.Bus-Stop/app.js
+++ HTTP and REST/Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,20 @@
 function getInfo() {
-    // TODO:
+    const busStopID = document.getElementById('stopId').value;
+    const stopNameContainer = document.getElementById('stopName');
+    const busContainer = document.getElementById('buses');
+    busContainer.replaceChildren();
+    const URL = `http://localhost:3030/jsonstore/bus/businfo/${busStopID}`;
+
+    fetch(URL, { method: 'GET' })
+        .then(res => res.json())
+        .then(data => {
+            stopNameContainer.textContent = data.name;
+
+            Object.entries(data.buses).forEach(([key, value]) => {
+                let newLi = document.createElement('li');
+                newLi.textContent = `Bus ${key} arrives in ${value} minutes`;
+                busContainer.appendChild(newLi);
+            })
+        })
+        .catch(err => stopNameContainer.textContent = 'Error');
 }
