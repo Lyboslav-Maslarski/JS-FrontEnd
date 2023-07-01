68ab82e7c56c3fd3b7fc4334866298e61615f879
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-07-01 07:26:07 +0300

    add

--- HTTP and REST/Exercise/04.Blog/app.js
+++ HTTP and REST/Exercise/04.Blog/app.js
@@ -1,5 +1,46 @@
 function attachEvents() {
-    // TODO:
+    const postsURL = 'http://localhost:3030/jsonstore/blog/posts';
+    const commentsURL = 'http://localhost:3030/jsonstore/blog/comments';
+    const btnLoadPosts = document.getElementById('btnLoadPosts');
+    const posts = document.getElementById('posts');
+    const title = document.getElementById('post-title');
+    const body = document.getElementById('post-body');
+    const btnViewPost = document.getElementById('btnViewPost');
+    const comments = document.getElementById('post-comments');
+    let allPosts;
+
+    btnLoadPosts.addEventListener('click', loadPosts);
+    btnViewPost.addEventListener('click', loadComments);
+
+    async function loadPosts() {
+        allPosts = await fetch(postsURL, { method: 'GET' });
+        allPosts = await allPosts.json();
+        for (const key in allPosts) {
+            let option = document.createElement('option');
+            option.value = key;
+            option.textContent = allPosts[key].title;
+            posts.appendChild(option);
+        }
+    }
+
+    async function loadComments() {
+        let currentPostId = posts.value;
+        body.textContent = allPosts[currentPostId].body;
+        title.textContent = allPosts[currentPostId].title;
+
+        let allComments = await fetch(commentsURL, { method: 'GET' });
+        allComments = await allComments.json();
+
+        comments.innerHTML = '';
+        Object.entries(allComments).forEach(([key, { id, postId, text }]) => {
+            if (currentPostId === postId) {
+                let li = document.createElement('li');
+                li.id = id;
+                li.textContent = text;
+                comments.appendChild(li);
+            }
+        });
+    }
 }
 
 attachEvents();
