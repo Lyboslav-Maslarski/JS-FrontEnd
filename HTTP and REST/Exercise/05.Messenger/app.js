c96b558da911dc25a5699982ce302ad3224963d3
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-07-01 07:46:11 +0300

    add

--- HTTP and REST/Exercise/05.Messenger/app.js
+++ HTTP and REST/Exercise/05.Messenger/app.js
@@ -1,5 +1,30 @@
 function attachEvents() {
-    // TODO:
+    const url = 'http://localhost:3030/jsonstore/messenger';
+    const textarea = document.getElementById('messages');
+    const authorInput = document.querySelector('input[name="author"]');
+    const contentInput = document.querySelector('input[name="content"]');
+    const submitBtn = document.getElementById('submit');
+    const refreshBtn = document.getElementById('refresh');
+
+    submitBtn.addEventListener('click', submitMessage);
+
+    async function submitMessage() {
+        let message = { author: authorInput.value, content: contentInput.value };
+        await fetch(url, { method: 'POST', body: JSON.stringify(message) });
+        loadAllMessages();
+    }
+
+    refreshBtn.addEventListener('click', loadAllMessages);
+
+    async function loadAllMessages() {
+        textarea.value = '';
+        let allMessages = await fetch(url, { method: 'GET' });
+        allMessages = await allMessages.json();
+        for (const key in allMessages) {
+            textarea.value += `${allMessages[key].author}: ${allMessages[key].content}\n`;
+        }
+        textarea.value = textarea.value.trim();
+    }
 }
 
 attachEvents();
