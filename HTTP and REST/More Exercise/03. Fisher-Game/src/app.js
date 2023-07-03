1884599c5506f6ea66b7f77a096476d6a1c2512e
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-07-04 02:17:59 +0300

    add

--- HTTP and REST/More Exercise/03. Fisher-Game/src/app.js
+++ HTTP and REST/More Exercise/03. Fisher-Game/src/app.js
@@ -1,3 +1,24 @@
-window.addEventListener('load', () => {
+let userData = JSON.parse(sessionStorage.getItem('userData'));
+
+if (userData !== null && userData.accessToken) {
+    document.getElementById('guest').style.display = 'none';
+    document.querySelector('.email > span').textContent = userData.email;
+} else {
     document.getElementById('user').style.display = 'none';
-})
+}
+
+const catches = document.getElementById('catches');
+const addForm = document.getElementById('addForm');
+catches.innerHTML = '';
+
+const logoutBtn = document.getElementById('logout');
+logoutBtn.addEventListener('click', logoutUser);
+
+async function logoutUser() {
+    await fetch('http://localhost:3030/users/logout', {
+        method: 'GET',
+        headers: { 'X-Authorization': userData.accessToken }
+    });
+    sessionStorage.clear();
+    window.location = 'index.html';
+}
--- HTTP and REST/More Exercise/03. Fisher-Game/src/register.js
+++ HTTP and REST/More Exercise/03. Fisher-Game/src/register.js
@@ -1,3 +1,49 @@
 document.getElementById('home').classList.remove('active');
-document.getElementById('user').style.display = 'none';
 document.getElementById('guest').children[1].classList.add('active');
+
+let userData = JSON.parse(sessionStorage.getItem('userData'));
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', registerUser);
+
+if (userData !== null && userData.accessToken) {
+    document.getElementById('guest').style.display = 'none';
+} else {
+    document.getElementById('user').style.display = 'none';
+}
+
+async function registerUser(e) {
+    e.preventDefault();
+    let [email, password, repeat] = Array.from(document.querySelectorAll('input'))
+        .map(i => i.value)
+        .map(v => v.trim());
+    const notification = document.querySelector('.notification');
+
+    try {
+        if (email === '' || password === '' || repeat === '') {
+            throw new Error('All fields are required!');
+        } else if (password !== repeat) {
+            throw new Error('Passwords don\'t match!');
+        }
+
+        let res = await fetch('http://localhost:3030/users/register', {
+            method: 'POST',
+            body: JSON.stringify({ email, password })
+        });
+
+        if (res.status !== 200) {
+            let error = await res.json();
+            throw new Error(error.message);
+        }
+
+        res = await res.json();
+
+        userData = { email, password, accessToken: res.accessToken, id: res._id };
+
+        sessionStorage.setItem('userData', JSON.stringify(userData));
+
+        window.location = 'index.html';
+    } catch (err) {
+        notification.textContent = err.message;
+    }
+}
