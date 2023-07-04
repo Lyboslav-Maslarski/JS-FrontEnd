0217810460e9ae19a10e8a48c59f1c8fc5036d73
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-07-04 04:29:25 +0300

    add

--- HTTP and REST/More Exercise/03. Fisher-Game/src/app.js
+++ HTTP and REST/More Exercise/03. Fisher-Game/src/app.js
@@ -1,20 +1,19 @@
 const url = 'http://localhost:3030/data/catches/';
 let userData = JSON.parse(sessionStorage.getItem('userData'));
+const catches = document.getElementById('catches');
+catches.innerHTML = '';
 
 if (userData !== null && userData.accessToken) {
     document.getElementById('guest').style.display = 'none';
     document.querySelector('.email > span').textContent = userData.email;
-    loadCatches();
+    document.querySelector('button.add').disabled = false;
 } else {
     document.getElementById('user').style.display = 'none';
 }
 
-const catches = document.getElementById('catches');
-const addForm = document.getElementById('addForm');
-catches.innerHTML = '';
-
-const logoutBtn = document.getElementById('logout');
-logoutBtn.addEventListener('click', logoutUser);
+document.getElementById('logout').addEventListener('click', logoutUser);
+document.querySelector('button.load').addEventListener('click', loadCatches);
+document.querySelector('button.add').addEventListener('click', addCatch);
 
 async function logoutUser() {
     await fetch('http://localhost:3030/users/logout', {
@@ -26,6 +25,8 @@ async function logoutUser() {
 }
 
 async function loadCatches() {
+    catches.innerHTML = '';
+
     let allCatches = await fetch(url, { method: 'GET' });
     allCatches = await allCatches.json();
 
@@ -37,17 +38,17 @@ async function loadCatches() {
 
         div.innerHTML = `
         <label>Angler</label>
-        <input type="text" class="angler" value="${c.angler}" disabled>
+        <input type="text" class="angler" value="${c.angler}">
         <label>Weight</label>
-        <input type="text" class="weight" value="${c.weight}" disabled>
+        <input type="text" class="weight" value="${c.weight}">
         <label>Species</label>
-        <input type="text" class="species" value="${c.species}" disabled>
+        <input type="text" class="species" value="${c.species}">
         <label>Location</label>
-        <input type="text" class="location" value="${c.location}" disabled>
+        <input type="text" class="location" value="${c.location}">
         <label>Bait</label>
-        <input type="text" class="bait" value="${c.bait}" disabled>
+        <input type="text" class="bait" value="${c.bait}">
         <label>Capture Time</label>
-        <input type="number" class="captureTime" value="${c.captureTime}" disabled>
+        <input type="number" class="captureTime" value="${c.captureTime}">
         `;
 
         let updateBtn = document.createElement('button');
@@ -76,6 +77,7 @@ async function loadCatches() {
 
 async function updateCatch(e) {
     let currentCatch = e.target.parentElement;
+    let id = e.target.getAttribute('data-id');
 
     let [angler, weight, species, location, bait, captureTime] = currentCatch.querySelectorAll('input');
 
@@ -88,7 +90,7 @@ async function updateCatch(e) {
         captureTime: captureTime.value
     }
 
-    await fetch(url + currentCatch.owner, {
+    await fetch(url + id, {
         method: 'PUT',
         headers: {
             'X-Authorization': userData.accessToken,
@@ -99,12 +101,37 @@ async function updateCatch(e) {
 }
 
 async function deleteCatch(e) {
-    let currentCatch = e.target.parentElement;
-    await fetch(url + currentCatch.owner, {
+    let id = e.target.getAttribute('data-id');
+    await fetch(url + id, {
         method: 'DELETE',
         headers: {
             'X-Authorization': userData.accessToken,
         }
+    });
+    loadCatches();
+}
+
+async function addCatch(e) {
+    e.preventDefault();
+    let inputForm = e.target.parentElement;
+
+    let [angler, weight, species, location, bait, captureTime] = inputForm.querySelectorAll('input');
+
+    let newCatch = {
+        angler: angler.value,
+        weight: weight.value,
+        species: species.value,
+        location: location.value,
+        bait: bait.value,
+        captureTime: captureTime.value
+    }
+
+    await fetch(url, {
+        method: 'POST',
+        headers: {
+            'X-Authorization': userData.accessToken,
+        },
+        body: JSON.stringify(newCatch)
     });
     loadCatches();
 }
