fdaf9873fa838e297c020579aa16b0d7680966b4
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-07-31 20:25:19 +0300

    add

---
+++ Exam Preparation/8 Apr 2023/02.Sprint-Planning/app.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', solve);
+
+let currentPoints = 0;
+const title = document.getElementById('title');
+const description = document.getElementById('description');
+const label = document.getElementById('label');
+const points = document.getElementById('points');
+const assignee = document.getElementById('assignee');
+const createTaskBtn = document.getElementById('create-task-btn');
+const deleteTaskBtn = document.getElementById('delete-task-btn');
+const tasksSection = document.getElementById('tasks-section');
+const totalSprintPoints = document.getElementById('total-sprint-points');
+const totalSpringPointsText=`Total Points ${currentPoints}pts`;
+
+function solve() {
+    // TODO:
+}
